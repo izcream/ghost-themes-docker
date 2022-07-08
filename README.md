f13cd6febd889c78555770e692e9275b756eea1f
izcream* <rabbit-dolls@hotmail.com>
2022-07-08 19:22:52 +0700

    🧹 chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ghost Blog themes development
---
+++ my-theme/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.hbs",
+    "**/*.hbs"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#f0fa55',
+        secondary: 'lightblue'
+      }
+    },
+  },
+  plugins: [],
+}
